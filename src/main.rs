e7ffdd79ffe0c44a496ad0b9de8062d495778052
Alex Richards <git@alxr.uk>
2025-05-07 01:58:39 +0100

    gather

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use mpi::traits::{Communicator, Destination, Source};
+use mpi::traits::{Communicator, Destination, Root, Source};
 
 fn main() {
     let universe = mpi::initialize().expect("Couldn't initialize universe!");
@@ -9,7 +9,18 @@ fn main() {
 
     if rank == 0 { // if we're the first process in the chain
         println!("Process {} sending number 2.", rank);
-        world.process_at_rank(rank + 1).send(&2);
+        world.process_at_rank(rank + 1).send(&2u32);
+
+        // initialise our array and allocate memory
+        let mut all_nums = vec![0u32; world.size() as usize];
+        // gather values
+        world.process_at_rank(0).gather_into_root(&2, &mut all_nums[..]);
+
+        println!("Root gathered sequence: {:?}.", all_nums);
+
+        // manually sum the values ourselves
+        let total: u32 = all_nums.iter().sum();
+        println!("Total is {}", total);
     } else {
         // could also use world.any_process().receive();
         let (number, _): (i32, _) = world.process_at_rank(rank - 1).receive();
@@ -23,5 +34,7 @@ fn main() {
             println!("Process {} sending {}.", rank, bigger);
             world.process_at_rank(rank + 1).send(&bigger);
         }
+
+        world.process_at_rank(0).gather_into(&bigger);
     }
 }
